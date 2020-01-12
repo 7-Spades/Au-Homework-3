28dbe81dc4b290505ff38db254ba2d0dfc8d5da2
7-Spades <edwin5106@gmail.com>
2020-01-12 15:26:55 -0600

    added if based random generator

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
     var lowAlph =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
     "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     var combo = [alphabet, lowAlph, numb, special]
+    console.log(alphabet, numb, special, lowAlph);
        
    // User input
     var promptMe= document.getElementById("action");
@@ -22,16 +23,18 @@
 
             if(howlong > " " && neednum === true && needspe === true){
                 console.log(howlong, neednum, needspe);
-                return howlong, neednum, needspe;
+                return startup, howlong, neednum, needspe;
             }else if(howlong > " " && neednum === true && needspe === false ){
                 console.log(howlong, neednum);
-                return howlong, neednum;
+                return startup, howlong, neednum;
             }else if(howlong > " " && needspe === true && neednum === false){
                 console.log(howlong, needspe)
-                return howlong, neednum;
-            }else{
+                return startup, howlong, neednum;
+            }else if(howlong > "" && neednum === false && neednum ===false){
                 console.log(howlong)
-                return howlong;
+                return startup, howlong;
+            }else{
+                alert("Invalid Input!")
             };
         
         };
@@ -43,4 +46,35 @@
         var ran= Math.floor(Math.random()*4);
         console.log(ran);
 
+        if(ran === 0){
+            var guess01= Math.floor(Math.random()*25); 
+            console.log(guess01, alphabet[guess01]);
+            var pen1 = document.getElementsByClassName("lead");
+            pen1[0].textContent= alphabet.indexOf(guess01);
+            document.body.appendChild(pen1[0]);
+        };
+
+        if(ran === 1){
+            var guess02= Math.floor(Math.random()*10); 
+            console.log(guess02, numb[guess02]);
+            var pen2 = document.getElementsByClassName("lead");
+            pen2[0].textContent= numb.indexOf(guess02);
+            document.body.appendChild(pen2[0]);
+        };
+
+        if(ran === 2){
+            var guess03= Math.floor(Math.random()*13); 
+            console.log(guess03, special[guess03]);
+            var pen3 = document.getElementsByClassName("lead");
+            pen3[0].textContent= numb.indexOf(guess01);
+            document.body.appendChild(pen3[0]);
+        };
+
+        if(ran === 3){
+            var guess04= Math.floor(Math.random()*25); 
+            console.log(guess04, lowAlph[guess04]);
+            var pen4 = document.getElementsByClassName("lead");
+            pen4[0].textContent= lowAlph.indexOf(guess04);
+            document.body.appendChild(pen4[0]);
+        };
     };
